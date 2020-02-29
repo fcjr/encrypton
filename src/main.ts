3621fcf66e20057b89a028dca8ac6045a2ebe1df
Frank Chiarulli Jr <frank@frankchiarulli.com>
2020-02-29 16:36:22 -0500

    set vibrant background

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, nativeTheme } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
 	app.quit();
 }
+
+nativeTheme.themeSource = 'dark';
 
 const createWindow = (): void => {
 	// Create the browser window.
@@ -13,7 +15,8 @@ const createWindow = (): void => {
 		width: 300,
 		resizable: false,
 		titleBarStyle: 'hiddenInset',
-		backgroundColor: '#2b3543',
+		vibrancy: 'sidebar',
+		transparent: true
 	});
 
 	// and load the index.html of the app.
